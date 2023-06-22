12819351472fe6d85552bf5e88de13dafd00b5ab
Ranaivoson <air_manitra@yahoo.fr>
2023-06-22 09:19:36 +0200

    predictbase

--- DotNetMachineLearning.Core/Constantes.cs
+++ DotNetMachineLearning.Core/Constantes.cs
@@ -3,6 +3,14 @@ namespace DotNetMachineLearning.Core
 {
     public static class Constantes
     {
+        public static class MachineLearningModel
+        {
+            public static string Regression = "Regression";
+            public static string MulticlassClassification = "MulticlassClassification";
+            public static string Classification = "Classification";
+
+        }
+
         public static string DelimiteurDoitExister = "Le délimiteur doit etre défini.";
 
         public static string FichierDoitExister = "Le ou les fichiers doivent etre spécifiés.";
--- DotNetMachineLearning.Core/PredictBase.cs
+++ DotNetMachineLearning.Core/PredictBase.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.ML;
 using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
 using System.Data.SqlClient;
 
 namespace DotNetMachineLearning.Core
 {
-    public abstract class PredictBase: TransformBase
+    public abstract class PredictBase : TransformBase
     {
+        protected virtual object TrainModel(object dataView, string[] features, RegressionCatalog regressionCatalog, string modelNameSaved)
+        {
+            var data = dataView as IDataView;
+            var pipelineEstimator = MLContext.Transforms
+                .Concatenate("Features", features)
+                .Append(regressionCatalog.Trainers.Sdca());
+
+            // Train model
+            var trainedModel = pipelineEstimator.Fit(data);
+            // Save model
+            MLContext.Model.Save(trainedModel, data.Schema, $"{modelNameSaved}.zip");
+
+            return trainedModel;
+        }
+
+        public virtual object Predict(string modelNameSaved)
+        {
+            return null;
+        }
     }
 }
